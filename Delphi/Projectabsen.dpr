80ddc393e0bbc9778b0d4a7a9d281a614066e8f5
ichwankm <ichwankm@gmail.com>
2019-01-21 16:51:53 +0700

    project delphi tol!

---
+++ Delphi/Projectabsen.dpr
@@ -0,0 +1,13 @@
+program Projectabsen;
+
+uses
+  Forms,
+  searchabsen in 'C:\Program Files (x86)\Borland\Delphi7\Projects\searchabsen.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
